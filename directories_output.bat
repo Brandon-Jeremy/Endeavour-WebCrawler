60be683634ec0b7aca14265866b323bc689d99aa
Brandon-Jeremy <brandonnader1@gmail.com>
2023-03-18 20:10:23 +0200

    Working subdomain search

---
+++ directories_output.bat
@@ -0,0 +1,7 @@
+https://ctflearn.com/dashboard
+https://ctflearn.com/event
+https://ctflearn.com/lab
+https://ctflearn.com/robots.txt
+https://ctflearn.com/stats
+https://ctflearn.com/support
+https://ctflearn.com/terms
