ec0971eda9a5f38200046b7fce70593236d1bb11
Brandon-Jeremy <brandonnader1@gmail.com>
2023-03-18 20:10:48 +0200

    working subdomain search

--- Endeavour.py
+++ Endeavour.py
@@ -48,7 +48,7 @@ def findDirs(cleanUrl: str):
     link = group1+group2
     print("Main link: "+link)
     num_valid = 0
-    with open('subdomains_output.bat', 'w') as subdomains_output:
+    with open('directories_output.bat', 'w') as directories_output:
         with open('filesinuse/dirs_dictionary.bat','r') as potential_dirs:
             for directory in potential_dirs:
                 directory = directory.rstrip()
@@ -57,33 +57,66 @@ def findDirs(cleanUrl: str):
                 print(fullURL)
                 r = requests.get(fullURL)
                 if(r.status_code >= 200 and r.status_code <300):
-                    subdomains_output.write(fullURL+"\n")
+                    directories_output.write(fullURL+"\n")
                     num_valid+=1
                 print(r.status_code)
     print(num_valid)
 
+def findSubDomains(cleanUrl: str):
+    testdomains = ['en','fr','sp']
+    httptest = "https://"
+    linktest = "wikipedia.org"
+    http = cleanUrl.group(1)
+    link = cleanUrl.group(2)
+    success = 0
+    with open('subdomains_output.bat','w') as subdomains_output:
+        with open('filesinuse\subdomains_dictionary.bat','r') as domains:
+            for domain in domains:
+                domain = domain.rstrip()
+                fullURL = http+"www."+domain+"."+link
+                print(f"Link: {fullURL}")
+                try:
+                    r = requests.get(httptest+domain+"."+linktest)
+                    r.raise_for_status()
+                    if(r.status_code>=200 and r.status_code<300):
+                        subdomains_output.write(httptest+domain+"."+linktest+"\n")
+                        success+=1
+                    print(r.status_code)
+                except requests.exceptions.RequestException as error:
+                    print(f"Connection error: {error}")
+                except requests.exceptions.HTTPError as error:
+                    #Do nothing
+                    print(f"HTTP Exception occured: {error} of code: ",r.status_code)
+                except Exception as error:
+                    #Do nothing
+                    print(f"Some other exception occured: {error}")
+                except NewConnectionError as e:
+                    print('Could not connect to server:', e)
+    print(success)
+
 def main():
     """#Check the arguments entered by the user"""
-    if len(sys.argv) < 2:
-        print("Enter a target website URL as an argument")
-        sys.exit()
-        exit(0)
-    elif len(sys.argv) > 2:
-        print("Too many arguments, enter only 1 for the target website URL")
-        sys.exit()
-        exit(0)
-    else:
-        """#Store argument if valid"""
-        print("Argument Passed!")
-        url = sys.argv[1]
-
+    # if len(sys.argv) < 2:
+    #     print("Enter a target website URL as an argument")
+    #     sys.exit()
+    #     exit(0)
+    # elif len(sys.argv) > 2:
+    #     print("Too many arguments, enter only 1 for the target website URL")
+    #     sys.exit()
+    #     exit(0)
+    # else:
+    #     """#Store argument if valid"""
+    #     print("Argument Passed!")
+    #     url = sys.argv[1]
+    url = "https://ctflearn.com"
     """#Clean the URL to try with subdomains"""
     cleanUrlPattern = r"(https?://)([\w.-]+)"
     cleanUrl = re.match(cleanUrlPattern, url)
     """#https://ctflearn.com"""
     # print(cleanUrl.group(2))
     """Expected Output: ctflearn.com"""
-    findDirs(cleanUrl)
+    # findDirs(cleanUrl)
+    findSubDomains(cleanUrl)
 
     time.sleep(1)
 
--- test.py
+++ test.py
@@ -1,21 +1,48 @@
 import requests
+from urllib3.exceptions import NewConnectionError
+
 
 link = "https://ctflearn.com"
+http = "https://"
+subdomain = ''
+link = "wikipedia.org"
+subdomains = ['en.','fr.','sp.']
+#Valid Valid Invalid
 
-with open('subdomains_output.bat', 'w') as subdomains_output:
-    with open('filesinuse/dirs_dictionary.bat','r') as potential_dirs:
-        for directory in potential_dirs:
-            directory = directory.rstrip()
-            print(directory)
-            fullURL = link+'/'+directory
-            print(fullURL)
-            r = requests.get(fullURL)
-            if(r.status_code >= 200 and r.status_code <300):
-                subdomains_output.write(fullURL)
+with open('subdomains_output.bat','w') as subdomains_output:
+    for subdomain in subdomains:
+        try:
+            r = requests.get(http+subdomain+link)
+            r.raise_for_status()
+            if(r.status_code>=200 and r.status_code<300):
+                subdomains_output.write(http+subdomain+link+"\n")
             print(r.status_code)
+        except requests.exceptions.RequestException as error:
+            print(f"Connection error: {error}")
+        except requests.exceptions.HTTPError as error:
+            #Do nothing
+            print(f"HTTP Exception occured: {error} of code: ",r.status_code)
+        except Exception as error:
+            #Do nothing
+            print(f"Some other exception occured: {error}")
+        except NewConnectionError as e:
+            print('Could not connect to server:', e)
 
 
-def fun():
+def workingDir():
+    with open('directories_output.bat', 'w') as directories_output:
+        with open('filesinuse/dirs_dictionary.bat','r') as potential_dirs:
+            for directory in potential_dirs:
+                directory = directory.rstrip()
+                print(directory)
+                fullURL = link+'/'+directory
+                print(fullURL)
+                r = requests.get(fullURL)
+                if(r.status_code >= 200 and r.status_code <300):
+                    subdomains_output.write(fullURL)
+                print(r.status_code)
+
+def functional():
     with open('subdomains_output.bat', 'w') as subdomains_output:
         with open('filesinuse/dirs_dictionary.bat','r') as potential_dirs:
             for linked in potential_dirs:
