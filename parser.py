beb92943e297c7a591291d4d93acc6cb5c1acf98
Brandon-Jeremy <brandonnader1@gmail.com>
2023-03-25 23:59:26 +0200

    Added ability to parse arguments passed from CLI

---
+++ parser.py
@@ -0,0 +1,15 @@
+import argparse
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description='Python script to find all directories, subdomains,'+ 
+    'and files in the source code.')
+
+    parser.add_argument("weblink",help="Enter Link to crawl and find weaknesses",type=str)
+
+    parser.add_argument("-o","--option",help="Select if you'd like to crawl directories (1), subdomains (2), "+
+    "href links from source code (3) or all of the mentioned (4)",choices=[1,2,3,4],type=int, default=1)
+
+    args = parser.parse_args()
+
+    return args
+
